ec6c424a27ecbdae1f4a28a3860182fa8c57eb8b
FerAB0403 <l21212322@tectijuana.edu.mx>
2025-10-17 21:25:28 -0700

    Add files via upload

---
+++ src/ExamenU2/ExamenU2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamenU2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VALIDANDO SINGLETON...");
+            ChecadorTrafico checador1 = ChecadorTrafico.Instancia;
+            ChecadorTrafico checador2 = ChecadorTrafico.Instancia;
+
+            Console.WriteLine(ReferenceEquals(checador1, checador2));
+
+            Taxi miTaxi = new Taxi("T-123-XYZ");
+            Console.ReadKey();
+            Console.Clear();
+
+            checador1.PonerEnRuta(miTaxi);
+
+            Console.ReadKey();
+        }
+    }
+}
